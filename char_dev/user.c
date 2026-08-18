9b47d867f5b0603660c6283f8f1c1fbf736f0a32
Dhanushya N.V <83385010+dhanushyanv@users.noreply.github.com>
2026-08-18 20:40:07 +0530

    Create user app for char dev

---
+++ char_dev/user.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+    int fd = open("/dev/mychar");
+    if(fd <0)
+    {
+      perror("Failed to open the device");
+      return 1;
+    }
+    printf("device opened successfully\n");
+    close(fd);
+    printf("device closed successfully\n");
+    return 0;
+}
