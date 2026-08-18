b9dbe8d29f09787b9e9fc92092c194f0616c84a0
Dhanushya N.V <83385010+dhanushyanv@users.noreply.github.com>
2026-08-18 21:06:36 +0530

    Create user app for char dev write

--- char_dev/user.c
+++ char_dev/user.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
 int main()
 {
     int fd = open("/dev/mychar", O_RDWR);
@@ -10,6 +12,18 @@ int main()
       return 1;
     }
     printf("device opened successfully\n");
+    
+    const char *message ="Hello from user";
+    ssize_t bytes_written;
+    bytes_written = write(fd,message,strlen(message));
+    if(bytes_written < 0)
+    {
+        perror("Failed to write");
+        close(fd);
+        return 1;
+    }
+    printf("bytes written %ld\n", bytes_written);
+    
     close(fd);
     printf("device closed successfully\n");
     return 0;
