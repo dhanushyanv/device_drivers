c7e9469dc2bc1814a95af9ed9e61cc1acd7bfcec
Dhanushya N.V <83385010+dhanushyanv@users.noreply.github.com>
2026-08-19 15:31:38 +0530

    user.c buffer opt

--- char_dev/user.c
+++ char_dev/user.c
@@ -13,7 +13,7 @@ int main()
     }
     printf("device opened successfully\n");
     
-    const char *message ="Hello from user";
+    const char *message ="Hello from user Dhanu";
     ssize_t bytes_written;
     bytes_written = write(fd,message,strlen(message));
     if(bytes_written < 0)
@@ -24,18 +24,35 @@ int main()
     }
     printf("bytes written %ld\n", bytes_written);
     
-    char buffer[1024];
-    ssize_t bytes_read;
-    bytes_read = read(fd,buffer,sizeof(buffer)-1);
-    if(bytes_read < 0)
+    //Full read 
+    // char buffer[1024];
+    // ssize_t bytes_read;
+    // bytes_read = read(fd,buffer,sizeof(buffer)-1);
+    // if(bytes_read < 0)
+    // {
+    //     perror("Failed to read");
+    //     close(fd);
+    //     printf("device closed successfully\n");
+    //     return 1;
+    // }
+    // buffer[bytes_read] = '\0';
+    // printf("bytes read %ld: %s\n", bytes_read, buffer);
+
+    //partial buffer read
+    char partial_buffer[6];
+    ssize_t bytes_read_partial;
+    while((bytes_read_partial = read(fd, partial_buffer, sizeof(partial_buffer)-1)) > 0)
+    {
+        partial_buffer[bytes_read_partial] = '\0';
+        printf("bytes read partial %ld: %s\n", bytes_read_partial, partial_buffer);
+    }
+    if(bytes_read_partial < 0)
     {
         perror("Failed to read");
         close(fd);
         printf("device closed successfully\n");
         return 1;
     }
-    buffer[bytes_read] = '\0';
-    printf("bytes read %ld: %s\n", bytes_read, buffer);
 
     close(fd);
     printf("device closed successfully\n");
